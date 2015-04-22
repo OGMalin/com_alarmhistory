4d69b4577d63d0551bd707bd225516c7abd5a26c
OGMalin <oddg@sjakk.biz>
2015-04-22 09:25:00 +0200

    *

--- administrator/components/com_alarmhistory/sql/uninstall.mysql.utf8.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS `#__alarmhistory_site`;
-DROP TABLE IF EXISTS `#__alarmhistory_section`;
--- build.cmd
+++ build.cmd
@@ -1,21 +1,9 @@
 REM 7z.exe a commandline compression app from 7-zip
-move /y media media_org
 del com_alarmhistory.zip
-mkdir admin
-mkdir site
-mkdir media
-xcopy /d /y /s media_org\com_alarmhistory\*.* media\*.*
-copy administrator\components\com_alarmhistory\alarmhistory.xml alarmhistory.xml
-xcopy /d /y /s administrator\components\com_alarmhistory\*.* admin\*.*
-xcopy /d /y /s components\com_alarmhistory\*.* site\*.*
-del admin\alarmhistory.xml
-..\7z a -Y com_alarmhistory.zip media
-rmdir /S /Q media
+move admin\alarmhistory.xml alarmhistory.xml
 ..\7z a -Y com_alarmhistory.zip *.xml
 ..\7z a -Y com_alarmhistory.zip admin
 ..\7z a -Y com_alarmhistory.zip site
-del alarmhistory.xml
-rmdir /S /Q admin
-rmdir /S /Q site
-move /Y media_org media
+..\7z a -Y com_alarmhistory.zip media
+move alarmhistory.xml admin\alarmhistory.xml
 
